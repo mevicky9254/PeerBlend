8b5a5a18b34b79744cd93d43f883774e81e7a40b
mevicky9254 <vikkichoudhary6528@gmail.com>
2023-10-12 17:25:25 +0530

    Created the repositories for each entity

--- PEERBLEND/src/main/java/com/PEERBLEND/repository/InterestRepository.java
+++ PEERBLEND/src/main/java/com/PEERBLEND/repository/InterestRepository.java
@@ -1,9 +1,11 @@
 package com.PEERBLEND.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.PEERBLEND.model.Interest;
 
+@Repository
 public interface InterestRepository extends JpaRepository<Interest,Long> {
 
 }
--- PEERBLEND/src/main/java/com/PEERBLEND/repository/MatchRepository.java
+++ PEERBLEND/src/main/java/com/PEERBLEND/repository/MatchRepository.java
@@ -1,9 +1,11 @@
 package com.PEERBLEND.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.PEERBLEND.model.Match;
 
+@Repository
 public interface MatchRepository extends JpaRepository<Match,Long> {
 
 }
